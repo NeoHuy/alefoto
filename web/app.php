9bb86c026f1e67d2f7a51e61e75828819c77ee7b
Minh Biken <minhbiken14@gmail.com>
2017-08-12 11:20:19 +0700

    remove app_dev.php plit app_dev.php and app.php

--- web/app.php
+++ web/app.php
@@ -1,13 +1,33 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 require __DIR__.'/../vendor/autoload.php';
+
+umask(0000);
+ini_set('memory_limit', '-1');
+
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) || PHP_SAPI === 'cli-server')
+) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
+
+
 if (PHP_VERSION_ID < 70000) {
     include_once __DIR__.'/../var/bootstrap.php.cache';
 }
 
-$kernel = new AppKernel('prod', false);
+if(in_array(getenv('APPLICATION_ENV'), ['production'])) {
+    $kernel = new AppKernel('prod', false);
+} else {
+    Debug::enable();
+    $kernel = new AppKernel('dev', true);
+}
+
 if (PHP_VERSION_ID < 70000) {
     $kernel->loadClassCache();
 }
